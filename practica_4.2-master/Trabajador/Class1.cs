d22dd28e3bae7182e0c86eb590a9498655489eeb
iborra17853 <iborra17853@iesmarenostrum.com>
2017-02-24 19:49:03 +0100

    Añadida la opción "casado"

---
+++ practica_4.2-master/Trabajador/Class1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajador
+{
+    /// <summary>
+    /// <para>Clase que representa a un trabajador de una empresa.</para>
+    /// <para>Los trabajadores pueden ser de las siguiente secciones:
+    /// <list type="bullet">
+    ///     <item>
+    ///         <description>Marketing</description>
+    ///     </item>
+    ///     <item>
+    ///         <description>Ventas</description>
+    ///     </item>
+    ///     <item>
+    ///         <description>Logística</description>
+    ///     </item>
+    /// </list>
+    /// </para>
+    /// </summary>
+    public class Trabajador
+    {
+        /// <summary>
+        /// Edad por defecto de jubilación del trabajador
+        /// </summary>
+        private const int EdadJubilacionDefecto = 67;
+
+        /// <summary>
+        /// Nombre del trabajador
+        /// </summary>
+        private string nombre;
+
+        /// <summary>
+        /// Apellidos del trabajador
+        /// </summary>
+        private string apellidos;
+
+        /// <summary>
+        /// Edad del trabajador
+        /// </summary>
+        private int edad;
+
+        private bool casado;
+
+        /// <summary>
+        /// Obtiene y devuelve el nombredel trabajador
+        /// </summary>
+        /// <value>El nombre</value>
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        /// <summary>
+        /// Obtiene y devuelve los apellidos del trabajador
+        /// </summary>
+        /// <value>Los apellidos</value>
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        /// <summary>
+        /// Obtiene y devuelve la edad del trabajador.
+        /// La edad debe estar entre 1 y 150
+        /// </summary>
+        /// <value>Edad del trabajador</value>
+        public int Edad
+        {
+            get { return edad; }
+            set
+            {
+                if (value >= 1 && value <= 150)
+                    edad = value;
+            }
+        }
+
+        /// <summary>
+        /// Calcula los años que le quedan al trabajador para su jubilación
+        /// </summary>
+        /// <remarks>Este método usa sobrecarga</remarks>
+        /// <returns>Los años que le quedan al trabajador para su jubilación</returns>
+        public int CalculoAnyosJubilacion()
+        {
+            return EdadJubilacionDefecto - edad;
+        }
+
+        /// <summary>
+        /// <para>Calcula los años que le quedan al trabajador para su jubilación. </para>
+        /// <para>El valor del parámetro <paramref name="edadJubilacion"/> debe ser menor que <see cref="EdadJubilacionDefecto"/> y mayor o
+        /// igual que la edad del trabajador</para>
+        /// </summary>
+        /// <remarks>Este método usa sobrecarga</remarks>
+        /// <param name="edadJubilacion">Edad personalizada de jubilación, distinta de la edad por defecto</param>
+        /// <returns>Los años que le quedan al trabajador para su jubilación</returns>
+        public int CalculoAnyosJubilacion(int edadJubilacion)
+        {
+            if (edadJubilacion < EdadJubilacionDefecto && edadJubilacion >= edad)
+                return edadJubilacion - edad;
+            else
+                return EdadJubilacionDefecto - edad;
+           
+        }
+        /// <summary>
+        /// Nos dirá si la persona esta casada o es soltera
+        /// </summary>
+       
+        public bool Casado
+        {
+            get { return casado; }
+            set { casado = value; }
+        }  
+      }
+    }
+
+
---
+++ practica_4.2-master/Trabajador/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajador
+{
+    public partial class Form1 : Form
+    {
+        Trabajador nuevoTrabajador = new Trabajador();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BBotonCrear_Click(object sender, EventArgs e)
+        {
+            nuevoTrabajador.Nombre = textBox1.Text;
+            nuevoTrabajador.Apellidos = textBox2.Text;
+            nuevoTrabajador.Edad = (int)nu1.Value;
+        }
+
+        private void BBotonJubilacion_Click(object sender, EventArgs e)
+        {
+            nuevoTrabajador.CalculoAnyosJubilacion();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult casamiento;
+            casamiento = MessageBox.Show("¿Esta usted casado?", "Valor", MessageBoxButtons.YesNo);
+            if (casamiento == DialogResult.Yes)
+            {
+                nuevoTrabajador.Casado = true;
+            }
+            else
+            {
+                nuevoTrabajador.Casado = false;
+            }
+        }
+    }
+}
